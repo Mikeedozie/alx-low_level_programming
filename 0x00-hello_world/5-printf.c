5f23cd93daf7a7870f3d5c062a05bd254ec18cab
Edozie <odigweedozie2@gmail.com>
2022-09-08 19:34:47 -0700

    Printf function in play

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This prints out a set of texts using only the printf func
+ * Return: This makes the code end
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

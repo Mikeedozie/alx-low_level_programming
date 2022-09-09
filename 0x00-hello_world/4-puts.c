6c51de2cf5df17fd9f4873f7f1679daae0c8a6c9
Edozie <odigweedozie2@gmail.com>
2022-09-08 17:50:11 -0700

    Added a comment to the function block

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* This code will print out a quote about programming */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");/* This code will print out a quote about programming */
 	return (0);
 }

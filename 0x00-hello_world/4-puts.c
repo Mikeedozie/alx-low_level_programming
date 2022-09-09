3a87f25467051e9ebcba808baf8ee6a28f49afd3
Edozie <odigweedozie2@gmail.com>
2022-09-08 17:54:36 -0700

    Fixed length of code line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");/* This code will print out a quote about programming */
+	/* This code will print out a quote about programming */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

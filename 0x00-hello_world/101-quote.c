576b0640da4f5f8b194162043c368a2738724083
Edozie <odigweedozie2@gmail.com>
2022-09-08 20:28:48 -0700

    Exit code 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * main - Print series of text that will return an exit code*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

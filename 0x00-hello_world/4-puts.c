bc707a011e39b304f4b291b82b196c7bdee7ff26
Edozie <odigweedozie2@gmail.com>
2022-09-08 19:26:45 -0700

    Quote of the day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ * main - This holds the code that prints a quote,
+ * description: this block exits after the message prints
+ * Return: This ends the function block
+ */
 int main(void)
 {
-	/* This code will print out a quote about programming */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

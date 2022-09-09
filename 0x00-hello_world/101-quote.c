425cafbd778495f9f4385765158579ee6677edd0
Edozie <odigweedozie2@gmail.com>
2022-09-08 20:30:55 -0700

    Updated comment on script and added a new line to make code readable

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Print series of text that will return an exit code*/
+ * main - Print series of text that will return an exit code
+ * Return: Sends a value of one*/
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }

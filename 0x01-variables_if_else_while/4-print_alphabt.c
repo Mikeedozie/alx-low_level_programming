b2d7439080718974c55d95c95f1786053a98952e
Edozie <odigweedozie2@gmail.com>
2022-09-10 07:29:35 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This function doesn't print some alphabets
+ * Return:This returns exit code of 0
+ */
+
+int main(void)
+{
+	int letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (letters != q && letters != e)
+			putchar(letters);
+	}
+	putchar('\n');
+	return (0);
+}

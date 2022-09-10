78ffc1b79c78bc09370467b77c409db4b3ebc538
Edozie <odigweedozie2@gmail.com>
2022-09-10 06:13:17 -0700

    Printing lower cases of letter a-z using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - this is where the code begins
+ * Return: This returns an exit code of 0
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = "a"; letters <= "z"; letters++)
+		putchar(letters)
+	return (0);
+}

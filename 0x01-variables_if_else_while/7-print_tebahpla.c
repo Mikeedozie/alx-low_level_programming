4b8bc12323357551119ff91eacc68df7575d394f
Edozie <odigweedozie2@gmail.com>
2022-09-10 08:54:31 -0700

    Reverse of alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This function reverses the alphabets
+ * Return: This returns an exit code of 0
+ */
+
+int main(void)
+{
+	char reverse = 'z';
+
+	for (; reverse <= 'a'; reverse--)
+		putchar(reverse);
+	putchar('\n);
+
+	return (0);
+}

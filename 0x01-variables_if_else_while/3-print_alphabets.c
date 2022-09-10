a06f803017ef67003a477f96a5b3ba801def411a
Edozie <odigweedozie2@gmail.com>
2022-09-10 07:17:14 -0700

    Combination of a-z and A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Start of the code
+ * Return: This ends the code
+ */
+
+int main(void)
+{
+	int small, capital;
+
+	for (small = 'a'; small <= 'z'; small++)
+		putchar(small);
+	for (capital = 'A'; capital <= 'Z'; capital++)
+		putchar(capital);
+	putchar('\n');
+}

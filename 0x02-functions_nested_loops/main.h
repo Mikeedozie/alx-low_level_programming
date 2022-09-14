9c444159c79d378780c9b02f31d938f7851f9192
Mikeedozie <90062196+Mikeedozie@users.noreply.github.com>
2022-09-14 20:57:11 +0000

    Fixed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 #ifndef MAIN
 #define MAIN
+#include <unistd.h>
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,10 +15,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-=======
-#include <unistd.h>
-#ifndef MAIN_H
-#define MAIN_H
+
 
 /**
  * putchar - This returns the _putchar
@@ -30,6 +27,6 @@ int put(void)
 {
 	return (write(1, "_putchar\n", 9));
 }
->>>>>>> bfb19ca0c48f7a1b35fbc87fdcf9bbff0edd9488
+
 
 #endif

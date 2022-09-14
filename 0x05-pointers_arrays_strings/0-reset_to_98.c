71a958fd3923d2ede9f28615dcf772d1900e0f00
Edozie <odigweedozie2@gmail.com>
2022-09-14 04:37:48 +0100

    Added a Readme file and attempted task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function takes in a 
+ * pointer as an integer and resets the value
+ * to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers, Arrays and Strings
+
+This project aims at covering the concepts of pointers, arrays and strings with the aid of 11 tasks in this directory in my repository.

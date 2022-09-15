0b34f15532c742778e9e5e841f8bc4595d6b2ded
Edozie <odigweedozie2@gmail.com>
2022-09-14 19:47:55 -0700

    Made some changes to the betty style of main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,16 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - Void type
+ * Return: No return for void type
+ * @i: This is the integer value
+ */
+
 void positive_or_negative(int i);
 
 
-void positi8ve_or_negative(int i)
+void positive_or_negative(int i)
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
@@ -14,7 +20,6 @@ void positi8ve_or_negative(int i)
 		printf("%d is zero\n", i);
 	else
 		printf("%d is negative\n", i);
-	return;
 }
 
 #endif /* MAIN_H */

a2250d9ed72da2c73541eb40ef007ac6f62108da
Mikeedozie <90062196+Mikeedozie@users.noreply.github.com>
2022-09-15 02:12:47 +0000

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * positive_or_negative  - This is where the whole
+ * main - This is where the whole
  * process begins
  * @i: This is the integer to test.
  * Return: This returns an exit code of 0
  */
 
-void positive_or_negative(int i)
+int main(int i)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
-	else
-		printf("%d is negative\n", i);
-	return;
+	positive_or_negative(i);
+	return (0);
 }

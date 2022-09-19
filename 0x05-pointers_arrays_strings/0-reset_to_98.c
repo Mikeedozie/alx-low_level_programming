d30f2bbb8951de48790dc9eb6b00a594d073925e
Edozie <odigweedozie2@gmail.com>
2022-09-20 00:25:20 +0100

    Fixed trailng whitespace in my code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - This function takes in a 
+ * reset_to_98 - This function takes in a
  * pointer as an integer and resets the value
  * to 98
  * @n: This is a pointer value.

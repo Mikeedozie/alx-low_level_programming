b26fd03b2d95aa8a91e13bd4f4868912a44734b3
Edozie <odigweedozie2@gmail.com>
2022-09-20 00:58:35 +0100

    Task 1 attempted

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
 /**
- * 
- *
- *
- *
- *
+ * swap_int - This function will swap the values of two 
+ * integers in the main.c file given by ALX
+ * @a: This is the first pointer value
+ * @b: This is the second pointer value
+ * @tmp: This holds one of the variables to swap
+ * Return: Void
  */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}

f39514726bd0b635909e8f84fc8d1b3fa3cd05d6
Edozie <odigweedozie2@gmail.com>
2022-09-20 02:11:04 +0100

    Updated main.h file with forward declaration and attempted task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - This prints the reverse of a given string
+ * @s: This is the string to be reversed
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	return (strrev(s));
+}

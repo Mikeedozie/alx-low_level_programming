08499949d77d7155c560540f11874ff14bfe4521
Edozie <odigweedozie2@gmail.com>
2022-09-20 01:31:22 +0100

    Fixed swap variables logic

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * swap_int - This function will swap the values of two 
+ * swap_int - This function will swap the values of two
  * integers in the main.c file given by ALX
  * @a: This is the first pointer value
  * @b: This is the second pointer value
- * @tmp: This is the temporary value
  * Return: Void
  */
 

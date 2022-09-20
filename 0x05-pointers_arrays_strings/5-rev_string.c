34a60b4fe4066e5c6c263870514f9c42b07e81bb
Edozie <odigweedozie2@gmail.com>
2022-09-20 06:10:02 +0100

    Attempted task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,25 @@
-/bin/bash: indent: command not found
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * main - check the code
+ * @s: String value to be evaluated
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i, l, j;
+	char c;
+	
+	l = strlen(s);
+	
+	for (i = 0, j = l-1; i <= j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+	printf("%s", s);
+}

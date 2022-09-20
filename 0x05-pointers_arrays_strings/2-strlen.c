b8c70c1ec3fc8d396d171f8d2a1b997f78924797
Edozie <odigweedozie2@gmail.com>
2022-09-20 01:57:15 +0100

    Attempted task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - This counts the number of char in s
+ * Return: gives out counted value
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

a4a5ca24928a7035ec639411a10da167025169bd
Edozie <odigweedozie2@gmail.com>
2022-09-20 03:06:37 +0100

    Attempted task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - This checks for even numbers in the given
+ * string
+ * @str: This is the character to check
+ * Return: Void
+ */
+
+void puts2(char, *str)
+{
+	if (i % 2 == O)
+		printf("%d", i;
+}

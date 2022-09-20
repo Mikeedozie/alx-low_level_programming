0985912c5716caec138e967b39d91d68bb55bf52
Edozie <odigweedozie2@gmail.com>
2022-09-20 06:14:28 +0100

    Fixed output on string passed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,11 +15,10 @@ void rev_string(char *s)
 	
 	l = strlen(s);
 	
-	for (i = 0, j = l-1; i <= j; i++, j--)
+	for (i = 0, j = l - 1; i <= j; i++, j--)
 	{
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
 	}
-	printf("%s", s);
 }

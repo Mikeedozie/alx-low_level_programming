84e064387009ae4e7a70c0cef4de3214ee46e642
Edozie <odigweedozie2@gmail.com>
2022-09-21 13:09:36 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - This function concatenates two strings
+ * @dest: parameter 1
+ * @src: parameter 2
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	printf("%s%s", dest, src);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Pointers
+This project covers more extensively on pointers arrays and strings

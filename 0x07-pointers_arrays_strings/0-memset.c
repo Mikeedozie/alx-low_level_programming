17466490b8cff6627407216e2d8fff497f8c1928
Edozie <odigweedozie2@gmail.com>
2022-09-26 22:34:08 +0100

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - This returns a pointer to the memory s
+ * Return: 0.
+ */
+
+char _memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n)
+	return (0);
+}

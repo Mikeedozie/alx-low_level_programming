eb6675f1519286ceebc82c47cee41d7a374c3f16
Edozie <odigweedozie2@gmail.com>
2022-09-28 23:42:46 +0000

    Attempted Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - This counts the string
+ * @s: String to be counted
+ * Return: Value of counted String.
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+}

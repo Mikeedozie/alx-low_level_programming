a39b991f8524600988263e985fdfa8a65ef750d5
Edozie <odigweedozie2@gmail.com>
2022-09-29 00:46:07 +0000

    Pushed files to use on laptop

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+/**
+ * int main(void)
+ * {
+ *    _puts_recursion("Puts with recursion");
+ *    return (0);
+ *}
+ */
+
+/**int main(void)
+ * {
+ *     _print_rev_recursion("\nColton Walker");
+ *     return (0);
+ * }
+ */
+
+/*int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}*/
+
+int main(void)
+{
+    int r;
+
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);
+    return (0);
+}

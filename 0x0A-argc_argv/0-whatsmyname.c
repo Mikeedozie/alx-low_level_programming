60656035665d4bf04f620a20fb693727fcf7a896
Edozie <odigweedozie2@gmail.com>
2022-09-30 16:03:26 +0000

    Finished task one, excluded gitignore and script file i created, I don't want a clustered repo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - This returns the name of the program
+ * @argc: Argument count
+ * @argv: Argument Vector
+ * Return: (0).
+ */
+
+
 int main(int argc, char *argv[])
 {
-	int i;
-
 	printf("%s", argv[0]);
 	return (0);
 }

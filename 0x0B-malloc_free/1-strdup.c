52ba803229116ba9f19ca058662569a2948c1fe9
Edozie <odigweedozie2@gmail.com>
2022-10-03 23:31:39 +0000

    Finished task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Takes in a string and duplicates it
+ *
+ * @str: string to duplicate
+ * Return: pointer to char.
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; i < str[i]; i++)
+		;
+
+	ptr = (char *)malloc(i * sizeof(char));
+
+	for (; j <= i; j++)
+		*(ptr + j) = str[j];
+
+	return (ptr);
+}

78afce31bcec367436583eaecceed7d2bfd4bf76
Edozie <odigweedozie2@gmail.com>
2022-10-04 23:02:38 +0000

    Added another free block

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,9 +36,9 @@ int **alloc_grid(int width, int height)
 				free(ptr[j]);
 		}
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
 	return (ptr);

f0c5b638beab85b20e565bb0e1ee41aa6de19b01
Edozie <odigweedozie2@gmail.com>
2022-10-06 23:47:19 +0000

    Attempted task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory
+ * @nmemb: no of block to allocate
+ *
+ * @size: size of the blocks
+ *
+ * Return: Pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int count;
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	str = malloc(nmemb * size);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (count = 0; count < str[count]; count++)
+	{
+		str[count] = 0;
+	}
+
+	return (str);
+}

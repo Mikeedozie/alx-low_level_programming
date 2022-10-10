e1e9c994249c7d43ae5f151d24b8ad35d3820058
Edozie <odigweedozie2@gmail.com>
2022-10-10 21:47:55 +0000

    Finished task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - print qualities of a dog
+ *
+ * @d: dog object
+ *
+ * Return: 0
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("nothing");
+	if (!(d->name))
+		d->name = "(nil)";
+	if (!(d->age))
+		d->age = 0;
+	if (!(d->owner))
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Name: %s\n", d->owner);
+}

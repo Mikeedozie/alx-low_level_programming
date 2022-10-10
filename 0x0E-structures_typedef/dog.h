cedd6d3fcabb9942e0ffb91400358407bf848926
Edozie <odigweedozie2@gmail.com>
2022-10-10 20:23:18 +0000

    Added documentation for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
-
+/**
+ * struct dog - This creates a dog structure
+ *
+ * @name: Name of dog
+ *
+ * @age: Dogs age
+ *
+ * @owner: Owner of dog
+ *
+ * Return: Nothing
+ */
 
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 

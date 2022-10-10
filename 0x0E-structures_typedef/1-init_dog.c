c968e2afc817113aa2a67e5def1866a76fb7a1a2
Edozie <odigweedozie2@gmail.com>
2022-10-10 21:10:49 +0000

    Changed dereferecing style

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,14 +12,12 @@
  *
  * @owner: Owner
  *
- * Return: d
+ * Return: Nothing
  */
-
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }

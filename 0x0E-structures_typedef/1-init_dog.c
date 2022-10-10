f45ec84cf6b614ac64a4412d9da13080a54f11da
Edozie <odigweedozie2@gmail.com>
2022-10-10 21:04:16 +0000

    Removed malloc for my C file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialize dog object using pointers
@@ -18,13 +19,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-
-	if (d == NULL)
-		return (NULL);
-	d->name = name;
+	(*d).name = name;
 	d->age = age;
 	d->owner = owner;
-
-	return (d);
 }

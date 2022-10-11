8e8a26079a0730cf92828dff2d952995eba85dbe
Edozie <odigweedozie2@gmail.com>
2022-10-11 09:02:45 +0000

    Finished task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * dog_t new_dog - creates another dog
+ *
+ * @name: Dogs name
+ *
+ * @age: Dogs age
+ *
+ * @owner: Dog owner
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+		return (NULL);
+	(*my_dog).name = name;
+	(*my_dog).owner = owner;
+
+	return (my_dog);
+}

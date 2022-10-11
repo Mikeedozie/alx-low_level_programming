a9d41435b99f61f8fa3e287ef6f97883efe5d621
Edozie <odigweedozie2@gmail.com>
2022-10-11 09:31:35 +0000

    added a free block

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates another dog
@@ -17,17 +18,40 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0; j = 0;
+
+	for (; name[i] != '\0'; i++)
+		;
+
+	for (; owner[j] != '\0'; j++)
+		;
+
 	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
-	{
 		return (NULL);
+
+
+	my_dog->name = malloc(sizeof(char) * (i + 1));
+	if (my_dog->name == NULL)
+	{
 		free(my_dog);
-	}	
-	(*my_dog).name = name;
-	(*my_dog).owner = owner;
+		return (NULL);
+	}
+
+	my_dog->owner = malloc(sizeof(char) * (j + 1));
+
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		return (NULL);
+	}
+
+	strcpy((*my_dog).name = name);
+	strcpy((*my_dog).owner = owner);
 	my_dog->age = age;
 
 	return (my_dog);

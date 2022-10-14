84c1a432bfc62331ed6c98cde3cb35b686492439
Edozie <odigweedozie2@gmail.com>
2022-10-14 01:04:44 +0000

    Attempted task 2, similar to task 1, but vary in terms of output format; numbers for former and strings for latter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - This prints out a string,
+ * followed by the separator provided
+ *
+ * @separator: This is the delimiter
+ * @n: number of strings to print
+ * Return: Void.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list values;
+
+	va_start(values, n);
+
+	for (; i < n; i++)
+	{
+		printf("%s", va_arg(values, char *));
+
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(values);
+}

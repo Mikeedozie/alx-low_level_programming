759b7fd67f9984a9dc047dfb36764e5dfc9a93cf
Edozie <odigweedozie2@gmail.com>
2022-10-15 00:59:41 +0100

    Fix on prototype

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,9 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
+ * @s: string
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *s))
 {
 	if (name == NULL || f == NULL)
 		return;
